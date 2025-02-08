fca15b8b847dff1d7114a7a2ad3ca550151daf2c
lefana22 <lefana.502231@sxcce.edu.in>
2025-02-08 11:15:40 +0530

    Add files via upload

---
+++ javamaven/src/main/java/com/sxcce/javamaven/Javamaven.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sxcce.javamaven;
+
+/**
+ *
+ * @author IT20
+ */
+import java.util.*;
+public class Javamaven {
+
+    public static void main(String[] args) {
+        Scanner Scan=new Scanner(System.in);
+        int a=Scan.nextInt();
+        int b=Scan.nextInt();
+        System.out.println("addition of two numbers"+(a+b));
+    }
+}
